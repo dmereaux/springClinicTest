9e6fa62c15b3f40e03c9e5bcb7ff7d027fd0f15c
mereaux <dominique.mereaux@qualifiez.fr>
2024-04-28 16:46:49 +0200

    eee

---
+++ src/main/java/com/company/modelimplementations/FindOwnersTest.java
@@ -0,0 +1,52 @@
+package com.company.modelimplementations;
+
+
+import com.company.FindOwners;
+import org.graphwalker.core.machine.ExecutionContext;
+import org.graphwalker.java.annotation.GraphWalker;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+/**
+ * Implements the model (and interface) FindOwnersSharedState
+ * The default path generator is Random Path.
+ * Stop condition is 100% coverage of all edges.
+ */
+@GraphWalker(value = "random(edge_coverage(100))")
+public class FindOwnersTest extends ExecutionContext implements FindOwners {
+
+    @Override
+    public void v_Owners() {
+        $(By.tagName("h2")).shouldHave(text("Owners"));
+        org.junit.Assert.assertTrue($$x("id('owners')/tbody/tr").size() >= 10);
+    }
+
+    @Override
+    public void e_AddOwner() {
+        $("body > div > a").click();
+    }
+
+    @Override
+    public void v_FindOwners() {
+        $("h2").shouldHave(text("Find Owners"));
+        $("h2").shouldBe(visible);
+    }
+
+    @Override
+    public void e_Search() {
+//        $("#search-owner-form .btn-default").click(); #search-owner-form > fieldset > div.form-actions > button
+    	$("#search-owner-form > fieldset > div.form-actions > button").click();
+    }
+
+    @Override
+    public void e_FindOwners() {
+        $("body > div > div > div > ul > li:nth-child(2) > a").click();
+    }
+
+    @Override
+    public void v_NewOwner() {
+    }
+}
